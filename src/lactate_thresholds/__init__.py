776493f24054b7429438be4f2e575e3bc855640f
Tim Van Erum <tim.vanerum@dataroots.io>
2024-12-07 16:01:13 +0100

    Fix linting issues

--- src/lactate_thresholds/__init__.py
+++ src/lactate_thresholds/__init__.py
@@ -1,2 +1,2 @@
-from lactate_thresholds.plot import lactate_intensity_plot
-from lactate_thresholds.process import clean_data, determine
+from lactate_thresholds.plot import lactate_intensity_plot as lactate_intensity_plot
+from lactate_thresholds.process import clean_data as clean_data, determine as determine
